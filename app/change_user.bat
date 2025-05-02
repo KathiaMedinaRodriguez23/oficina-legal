91f40cd20cc21c2d3f95d7496c41407fd28cfd09
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2025-05-02 10:14:32 -0500

    first commit

---
+++ app/change_user.bat
@@ -0,0 +1,5 @@
+@REM git config --global user.name "Alejandro Toshiro Escobedo Correa"
+@REM git config --global user.email toshiro.escobedo@intediperu.com
+@REM git config --global user.username "SthercroosAlejo"
+
+git config --global --replace-all user.name "Kathia Medina Rodriguez" && git config --global --replace-all user.email kmedinar1@upao.edu.pe && git config --global --replace-all user.username "KathiaMedinaRodriguez23"
